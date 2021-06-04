ec6ab397b0c81f568b293f2725f48c81332cf2d8
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2021-06-04 11:44:16 +0300

    Push pop

--- lib/main.dart
+++ lib/main.dart
@@ -12,53 +12,70 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FirstPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class FirstPage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routing App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'First Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            TextButton(
+              child: Text('Go to second'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => SecondPage(data: 'Hello there from the first page!'),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SecondPage extends StatelessWidget {
+  final String data;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  SecondPage({
+    required this.data,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Routing App'),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Second Page',
+              style: TextStyle(fontSize: 50),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              data,
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
